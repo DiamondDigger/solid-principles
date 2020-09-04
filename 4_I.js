d7a31c2a4ce50b7e714f50f06b101543436c82ec
naglec13 <naglec13@gmail.com>
2020-09-04 23:52:54 +0300

    - #4 - violate principle - instances of classes in connection with useless methods from base class

--- 4_I.js
+++ 4_I.js
@@ -1,1 +1,51 @@
 //Interface segregation principle
+
+class Animal{
+    constructor(name){
+        this.name = name
+    }
+    walk(){
+        console.log(`${this.name} can walk`)
+    }
+    swim(){
+        console.log(`${this.name} can swim`)
+    }
+    fly(){
+        console.log(`${this.name} can fly`)
+    }
+}
+
+class Dog extends Animal{
+    fly() {
+        return null
+    }
+}
+class Eagle extends Animal{
+    swim() {
+        return null
+    }
+}
+class Whale extends Animal{
+    walk() {
+        return null
+    }
+    fly() {
+        return null
+    }
+}
+
+const dog = new Dog('Jimbo')
+const eagle = new Eagle('Desert Eagle')
+const whale = new Whale('Big Brother')
+
+dog.walk()
+dog.swim()
+dog.fly()
+
+eagle.walk()
+eagle.swim()
+eagle.fly()
+
+whale.walk()
+whale.swim()
+whale.fly()
