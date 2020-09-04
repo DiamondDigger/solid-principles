9fa43a68757afe16f615a75804b7a0625752c549
naglec13 <naglec13@gmail.com>
2020-09-04 18:55:51 +0300

    - #3 - violate principle - added three classes that extends base one (third one shouldn't have access to the base method)

--- 3_L.js
+++ 3_L.js
@@ -1,1 +1,28 @@
 //Liskov substitution principle
+
+class Person{
+    name = 'person'
+    access(name){
+        console.log(`${name} - You have access!`)
+    }
+}
+
+class FrontendDeveloper extends Person{
+    name = 'frontend developer'
+}
+
+class BackendDeveloper extends Person{
+    name = 'backend developer'
+}
+
+class Enemy extends Person{
+    name = 'I am an enemy, give me your access and I gonna destroy your system!!'
+}
+
+const front = new FrontendDeveloper()
+const back = new BackendDeveloper()
+const enemy = new Enemy()
+
+front.access(front.name)
+back.access(back.name)
+enemy.access(enemy.name)
