bc43f55ae2e7d21ba58f1ef72759110a77444d7c
naglec13 <naglec13@gmail.com>
2020-09-04 11:57:00 +0300

    - #1 - added single class

--- 1_S.js
+++ 1_S.js
@@ -1,1 +1,16 @@
 //Single responsibility principle
+
+class News {
+    constructor(title, text) {
+        this.title = title
+        this.text = text
+        this.modified = false
+    }
+
+    update(text) {
+        this.text = text
+        this.modified = true
+    }
+}
+
+
