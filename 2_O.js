c5da47e0d37166ecab58631d9ce0e5447adf9e0c
naglec13 <naglec13@gmail.com>
2020-09-04 15:48:44 +0300

    #2 - added several varieties of shape classes and Calculator class to calculate sum of areas

--- 2_O.js
+++ 2_O.js
@@ -1,1 +1,41 @@
 //Open close principle
+class Square {
+    constructor(size){
+        this.type = 'square'
+        this.size = size
+    }
+}
+
+class Circle{
+    constructor(radius){
+        this.type = 'circle'
+        this.radius = radius
+    }
+}
+
+class Calculator{
+    constructor(shapes = []){
+        this.shapes = shapes
+    }
+
+    sum(){
+        return this.shapes.reduce((acc, shape)=>{
+            switch (shape.type) {
+                case 'square':
+                    acc += shape.size ** 2
+                    break
+                case 'circle':
+                    acc += (shape.radius ** 2) * Math.PI
+                    break
+            }
+            return acc
+        },0)
+    }
+}
+
+const calculator = new Calculator([
+    new Square(10),
+    new Circle(1)
+])
+
+console.log(calculator.sum())
