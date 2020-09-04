05a2dfe17ce9ffc82acc93b1cec509e21ecfe825
naglec13 <naglec13@gmail.com>
2020-09-04 12:24:38 +0300

    - #1 - violate principle: added two methods to present instance of class in different types

--- 1_S.js
+++ 1_S.js
@@ -11,6 +11,28 @@ class News {
         this.text = text
         this.modified = true
     }
+
+    toHtml() {
+        return `
+        <div>
+            <h1>${this.title}</h1>
+            <p>${this.text}</p>
+        </div>
+        `
+    }
+
+    toJSON() {
+        return JSON.stringify({
+            title: this.title,
+            text: this.text,
+            modified: this.modified
+        },null, 1)
+    }
 }
+
+const news = new News('Sun - undiscovered planet', 'New research on New Zealand scientists')
+console.log(news)
+console.log('HTML presentation of news',news.toHtml())
+console.log('JSON presentation of news',news.toJSON())
 
 
