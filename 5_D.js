369ea331838264a6c06c84f61b96bff011e8d567
naglec13 <naglec13@gmail.com>
2020-09-05 16:03:10 +0300

    #5 - principle observed - database has no dependency from lower level realization

--- 5_D.js
+++ 5_D.js
@@ -1,1 +1,45 @@
 //Dependency inversion principle
+
+class Fetch{
+    request(url) {
+        return Promise.resolve('data from fetch ')
+    }
+}
+
+class LocalStorage{
+    get(){
+        const dataFromStorage = 'data from local storage'
+        return dataFromStorage
+    }
+}
+
+class FetchClient{
+     constructor(){
+         this.fetch = new Fetch()
+     }
+     clientGet(url){
+        return this.fetch.request(url)
+     }
+}
+
+class LocalStorageClient{
+    constructor(){
+        this.localStorage = new LocalStorage()
+    }
+    clientGet(key){
+        return this.localStorage.get(key)
+    }
+}
+
+class Database{
+    constructor(client){
+        this.client = client
+    }
+
+    getData(key){
+        return this.client.clientGet(key)
+    }
+}
+
+const db = new Database(new LocalStorageClient())
+console.log(db.getData('something could be there'))
